48fd82676970c5b8562162f0734b51f524d3ec0b
Bruno Félix de Sousa <79954751+BrunoFelixDeSousa@users.noreply.github.com>
2026-09-25 13:59:24 -0300

    fix: renamed name class

--- src/test/java/dev/brunofelix/domain/enums/UserStatusTest.java
+++ src/test/java/dev/brunofelix/domain/enums/UserStatusTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 @DisplayName("UserStatus")
-class UserStatus {
+class UserStatusTest {
 
     @Nested
     @DisplayName("When checking authentication")
